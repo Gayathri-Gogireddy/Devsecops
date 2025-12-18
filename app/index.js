70b4d092c55c7300204037cd24a0a9b92146522c
Gayathri-Gogireddy <gogireddy.gayathri2002@gmail.com>
2025-12-18 13:11:48 +0530

    second one

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevSecOps Pipeline Running 🚀");
+  res.send("DevSecOps Pipeline Runninggg 🚀");
 });
 
 app.listen(3000, () => {
